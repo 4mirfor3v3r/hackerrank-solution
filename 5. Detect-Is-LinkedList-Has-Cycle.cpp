0047170ded8f78fce6b993e0e27ab53643db94ee
ahmad_amirudin <ahmadamirudin901@gmail.com>
2022-11-19 22:30:13 +0700

    Complete Detect List Has Cycle

---
+++ 5. Detect-Is-LinkedList-Has-Cycle.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+        SinglyLinkedListNode *tail;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+            }
+
+            this->tail = node;
+        }
+};
+
+void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the has_cycle function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+bool has_cycle(SinglyLinkedListNode* head) {
+    SinglyLinkedListNode *p;
+    p=head;
+    while (p!=NULL) {
+        if(p->data== -11){
+            return true;
+        }
+        p->data=-11;
+        p=p->next;
+    }
+    return false;
+}
+
+// Using Floyd Warshal algorithm
+bool anotherHasCycleSolution(SinglyLinkedListNode* head) {
+    SinglyLinkedListNode* fast=head, *slow=head;
+    while(slow != NULL && fast != NULL && fast->next !=NULL){
+        slow=slow->next;
+        fast=fast->next->next;
+        if(slow == fast){
+            return true;
+        }
+    }
+    return false;
+}
